64105271a8a45428a6d1b69daa1f67bdd1845624
Sauce77 <sauceortediego505@gmail.com>
2025-09-25 15:14:47 -0600

    se agrego curp,clave y estatus

--- contactos/models.py
+++ contactos/models.py
@@ -18,9 +18,10 @@ class Seccion(models.Model):
     """
         Identifica una seccion del distrito.
     """
-    numero = models.IntegerField(null=False, blank=False)
+    numero = models.IntegerField(primary_key=True, unique=True, null=False, blank=False)
     nombre = models.CharField(max_length=100, null=True, blank=True)
     rentabilidad = models.FloatField(null=True, blank=True)
+    comentarios = models.TextField(null=True, blank=True)
 
     zonales = models.ManyToManyField(Zonal, null=True, blank=True)
 
@@ -32,12 +33,20 @@ class Contacto(MPTTModel):
     """
         Identifica a una persona dentro de la red de contactos del sistema.
     """
+    ESTATUS_CHOICES = {
+        'A': 'Afiliado',
+        'D': 'Desafiliado'
+    }
+
     nombre = models.CharField(max_length=150, null=False, blank=False)
     apellido_paterno = models.CharField(max_length=150, null=False, blank=False)
     apellido_materno = models.CharField(max_length=150, null=False, blank=False)
     telefono = models.CharField(max_length=10, null=True, blank=True)
     email = models.CharField(max_length=150, null=True, blank=True)
     domicilio = models.TextField(null=True, blank=True)
+    curp = models.CharField(max_length=18, blank=True, unique=True)
+    clave_elector = models.CharField(max_length=13, blank=True, unique=True)
+    estatus = models.CharField(max_length=1, choices=ESTATUS_CHOICES, default='A')
     
     seccion = models.ForeignKey(Seccion, on_delete=models.SET_NULL, null=True, blank=True)
 
