96c3a70e25d091f91e2c7db3cff5cf0ce2b6d9ab
Sauce77 <sauceortediego505@gmail.com>
2025-09-29 11:34:21 -0600

    se inserto color chart

--- static/scripts/contactos/organigrama.js
+++ static/scripts/contactos/organigrama.js
@@ -11,6 +11,7 @@ function drawChart() {
       dataTable.addColumn('string', 'Name');
       dataTable.addColumn('string', 'Manager');
       dataTable.addColumn('string', 'ToolTip');
+      dataTable.addColumn({type: 'string', role: 'style'}); 
       
       let allRows = [];
       processNode(data, allRows, ''); 
@@ -50,15 +51,26 @@ function handleNodeClick(chart, dataTable) {
 
 // Función recursiva (la que ya tenías)
 function processNode(node, allRows, parentId) {
+
   const nodeData = {
     'v': node.id.toString(), 
-    'f': `<b>${node.nombre} ${node.apellido_paterno}</b><br><span>${node.apellido_materno}</span>`
+    'f': `<b>${node.nombre} ${node.apellido_paterno}</b><br><span>${node.apellido_materno}</span>`,
   };
-  
+
+  let styleString = '';
+  if (node.estatus === 'A') {
+      styleString = 'background-color: #4CAF50; background-image: none; color: white;'; // Verde
+  } else if (node.estatus === 'D') {
+      styleString = 'background-color: #ff0707ff; background-image: none;'; // Amarillo
+  } else {
+      styleString = 'background-color: #2196F3; background-image: none;'; // Azul por defecto
+  }
+
   allRows.push([
     nodeData,
     parentId,
     `Contactos: ${node.descendant_count}`,
+    styleString,
   ]);
   
   if (node.children && node.children.length > 0) {
